400d4e5edf80795e97cf5d001fe503f937ed6b36
Enock Abere <maebaenock95@gmail.com>
2021-06-25 16:08:25 +0300

    add scroll to top js function

--- static/js/main.js
+++ static/js/main.js
@@ -8,3 +8,24 @@ $(document).ready(function(){
         }, 4000);
     });
 });
+//scroll top
+var scrollbut = document.getElementById("myBtn"); //get button to scroll up
+
+//when user scrolls down up to 20px show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction (){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        scrollbut.style.display = "block";
+    }else {
+        scrollbut.style.display = "none";
+    }
+}
+
+//when user clicks on the button it scrolls to top
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
